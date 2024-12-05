9d46347dbe8d142f45073fdc09737ac215834ac2
surajsingh0 <suraj752305@gmail.com>
2024-12-06 01:59:23 +0530

    update projects

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const myProjects = [
             "GHP (Goal Habit Progression) is an intuitive platform designed to help users track their habits and achieve their goals. The application features comprehensive habit tracking, goal progression with customizable milestones, streaks and rewards to boost motivation, and a mobile app for on-the-go access. It offers a range of plans from a free basic option to premium Pro and Team packages, catering to individual users and collaborative teams alike.",
         techStack: ["JavaScript", "Flask"],
         links: {
-            demo: "http://ghptrack.com/",
+            demo: "https://ghptrack.com/",
             repository: "https://github.com/surajsingh0/ghp-tracker",
         },
         image: "ghp.webp",
